b69123a480fe4c9e18b32cf31fabcbaca5240ca6
Mohamed EL_HOSAYNY <mohamed.elhosayny2@gmail.com>
2021-11-22 14:00:00 +0100

    Add domain models

---
+++ src/Conduit.Domain/Entities/Comment.cs
@@ -0,0 +1,13 @@
+﻿using Conduit.Domain.Models.Shared;
+
+namespace Conduit.Domain.Models
+{
+    public class Comment : BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string Body { get; set; }
+        public User Author { get; set; }
+    }
+}
---
+++ src/Conduit.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using Conduit.Domain.Models.Shared;
+
+namespace Conduit.Domain.Models
+{
+    public class User : BaseEntity
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Bio { get; set; }
+        public string Image { get; set; }
+        public ICollection<int> FavoriteArticleIds { get; set; }
+        public ICollection<int> FollowerIds { get; set; }
+    }
+}
